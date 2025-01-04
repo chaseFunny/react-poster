8d40c1cc1a86ad83dfe605469fefa466cdd74d04
snailRun <snailrun160@gamil.com>
2025-01-04 18:17:04 +0800

    chore: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 const App = () => {
+	const [count, setCount] = useState(0);
 	return (
 		<div className="content">
 			<h1 className="bg-red-500">Rsbuild with React</h1>
-			<p>Start building amazing things with Rsbuild.</p>
+			<p>Start building amazing things with Rsbuild. count is：{ count}</p>
+			<Button onClick={() => setCount(pre => pre + 1)}> click me</Button>
 		</div>
 	);
 };
